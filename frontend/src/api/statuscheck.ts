abf157ceb26e5bdec65a900234606004876f5b6b
Pereki <ruben.elias.kraft@outlook.de>
2026-07-25 20:53:18 +0200

    fix: use correct port in frontend

--- frontend/src/api/statuscheck.ts
+++ frontend/src/api/statuscheck.ts
@@ -2,13 +2,13 @@ import type { Event } from '@/api/types'
 import { type Ref, ref } from 'vue'
 
 export async function checkStatus (): Promise<Event> {
-    const response = await fetch('http://localhost:3000/api/status')
+    const response = await fetch('http://localhost:80/api/status')
     const data = await response.json()
     return data as Event
 }
 
 export function connectWebSocket () {
-    const socket = new WebSocket('ws://localhost:3000/ws')
+    const socket = new WebSocket('ws://localhost:80/ws')
 
     const status: Ref<Event | undefined> = ref(undefined)
     socket.addEventListener('open', () => {
--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pnpm --dir ./frontend/sysinfo-frontend run build
-mv ./frontend/sysinfo-frontend/dist ./frontend-dir
+mv ./frontend/dist ./frontend-dir
 cargo run
